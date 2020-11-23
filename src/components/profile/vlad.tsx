6cc77f20c4d3f229bf665c255e7b97b6f9ee05ae
Vlad756 <v.chelyadin2015@yandex.ru>
2020-11-24 01:19:26 +0300

    Added Vlad's profile

---
+++ src/components/profile/vlad.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const VladProfile = () => {
+
+    return (
+        <Fragment>
+            <h1>Vlad's Profile</h1>
+        </Fragment>
+    );
+}
+
+export default VladProfile;

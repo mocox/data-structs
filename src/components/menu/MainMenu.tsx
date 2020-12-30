63ae013d996b51f9b7743461d1e4511495e21c79
mocox <mo_cox@msn.com>
2020-12-30 12:27:10 +0000

    removed unused imports

--- src/components/menu/MainMenu.tsx
+++ src/components/menu/MainMenu.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import { Button, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 import classes from './mainmenu.module.css';
 

dcb01e9c7b1f8b2842071c7cb16b7afe96d43343
mocox <mo_cox@msn.com>
2020-12-30 12:20:59 +0000

    main menu fixes for not reloading page

--- src/components/menu/MainMenu.tsx
+++ src/components/menu/MainMenu.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
+
+import { Button, Nav, Navbar, NavDropdown} from 'react-bootstrap';
 
 import classes from './mainmenu.module.css';
 
@@ -13,27 +15,27 @@ const MainMenu = (props: any) => {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
                 <NavDropdown title="Intros" id="intros-dropdown" className={classes.DropdownContainer}>
-                    <NavDropdown.Item href="/intros/big-o">Big O</NavDropdown.Item>
-                    <NavDropdown.Item href="/intros/logarithms">Log<sub>2</sub>N</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/intros/big-o">Big O</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/intros/logarithms">Log<sub>2</sub>N</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Searches" id="searches-dropdown">
-                    <NavDropdown.Item href="/searches/linear">Linear Search</NavDropdown.Item>
-                    <NavDropdown.Item href="/searches/binary">Binary Search</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/searches/linear">Linear Search</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/searches/binary">Binary Search</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Sorting" id="sorting-dropdown">
-                    <NavDropdown.Item href="/sorting/bubble">Bubble Sort</NavDropdown.Item>
-                    <NavDropdown.Item href="/sorting/selection">Selection Sort</NavDropdown.Item>
-                    <NavDropdown.Item href="/sorting/insertion">Insertion Sort</NavDropdown.Item>
-                    <NavDropdown.Item href="/sorting/merge">Merge Sort</NavDropdown.Item>
-                    <NavDropdown.Item href="/sorting/quick">Quick Sort</NavDropdown.Item>
-                    <NavDropdown.Item href="/sorting/radix">Radix Sort</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/sorting/bubble">Bubble Sort</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/sorting/selection">Selection Sort</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/sorting/insertion">Insertion Sort</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/sorting/merge">Merge Sort</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/sorting/quick">Quick Sort</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/sorting/radix">Radix Sort</NavDropdown.Item>
                 </NavDropdown>
                 <NavDropdown title="Data Structures" id="structs-dropdown">
-                    <NavDropdown.Item href="/data-structures/singleLL">Single Linked List</NavDropdown.Item>
-                    <NavDropdown.Item href="/data-structures/doubleLL">Double Linked List</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/data-structures/singleLL">Single Linked List</NavDropdown.Item>
+                    <NavDropdown.Item as={NavLink} to="/data-structures/doubleLL">Double Linked List</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="/profile">Profile</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link as={NavLink} to="/profile">Profile</Nav.Link>
+                <Nav.Link as={NavLink} to="/about">About</Nav.Link>
                 </Nav>
                 <Nav>
                     <span className={classes.Version}>Version : {version}</span>

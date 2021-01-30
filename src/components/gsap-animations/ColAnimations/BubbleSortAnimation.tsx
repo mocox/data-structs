ed9ef1faa4c7bd094023acddc039aed58504e68d
Vlad756 <v.chelyadin2015@yandex.ru>
2021-01-30 16:00:40 +0300

    liveshare

--- src/components/gsap-animations/ColAnimations/BubbleSortAnimation.tsx
+++ src/components/gsap-animations/ColAnimations/BubbleSortAnimation.tsx
@@ -1,6 +1,7 @@
 import gsap from 'gsap';
 import React, { Fragment, RefObject, useEffect, useState } from 'react';
 import { FormGroup } from 'react-bootstrap';
+import { createIndexedAccessTypeNode } from 'typescript';
 
 import classes from './BubbleSortAnimation.module.css';
 
@@ -11,22 +12,72 @@ const BubleSortAnimation = (props: any) => {
     const items:RefObject<HTMLDivElement>[] = [];
     const inputRef: RefObject<HTMLInputElement> = React.createRef();    
 
+    const BubbleSort = () => {
+
+        const els = document.getElementsByClassName('arrayItem');
+        
+        let index = 0;
+        while (index < elements.length-1) {
+            
+            
+            for (let j = 0; j < els.length-1; j ++) {
+
+                //get current element
+                const currentElement = els[j];
+                const elIndex = currentElement.id.split('_')[1];
+
+                // current
+                const numElement = document.getElementById('num_' + elIndex);
+                const value: number = Number.parseInt(numElement?.innerHTML ??'0');
+                
+                // next
+                const nextElement = els[j+1];
+                const nextNumElement = document.getElementById('num_' + j+1);
+                const nextValue: number = Number.parseInt(nextNumElement?.innerHTML ?? '0');
+                
+                console.log('currentvalue - nextValue -> ', value, nextValue);
+                if (value > nextValue) {
+                    Swap(currentElement, nextElement, els, j);
+                }
+            }
+
+            // index = elements.length;
+            index ++;
+        }
+
+        
+    }
+
+    const Swap = (a: Element, b: Element, els: any, currentIndex: number) => {
+
+        console.log('swapping -> index ->', currentIndex);
+        
+        
+        gsap.to(b, {x:-35, duration: 1});
+        gsap.to(a, {x:35, duration: 1});
+
+        for (let i = currentIndex + 1; i < els.length; i++){
+            gsap.to(els[i], {x:0, duration: 1, delay: 1});
+        }
+        
+    }
+    
     useEffect(()=>{
         
         gsap.from('.arrayItem', { y: -800, duration: 1, ease: 'bounce', opacity:0, stagger: 0.1 });
 
     },[]);
 
-    useEffect(() => {
+    // useEffect(() => {
 
-        // id of last element
+    //     // id of last element
 
-        const itemId: string = '#item_' + (elements.length - 1);
-        const id: string = '#block_' +  (3);
-        gsap.fromTo(itemId, { x: -400, duration: 0.1, opacity:0 }, { x: 0, duration: 1, delay:1, opacity:1 }); 
-        gsap.to(id,{ height: '100px', duration: 5, delay: 0.1});   
+    //     const itemId: string = '#item_' + (elements.length - 1);
+    //     const id: string = '#block_' +  (3);
+    //     gsap.fromTo(itemId, { x: -400, duration: 0.1, opacity:0 }, { x: 0, duration: 1, delay:1, opacity:1 }); 
+    //     //gsap.to(id,{ height: '100px', duration: 5, delay: 0.1});   
 
-    },[elements]);
+    // },[elements]);
 
     const changed = (event: any) => {
         
@@ -39,11 +90,15 @@ const BubleSortAnimation = (props: any) => {
 
             setElements(ints ?? []);
 
-        gsap.from('.arrayItem', { y: -800, duration: 1, ease: 'bounce', opacity:0, stagger: 0.1 });
+            //gsap.from('.arrayItem', { y: -800, duration: 1, ease: 'bounce', opacity:0, stagger: 0.1 });
+
+            BubbleSort();
 
         }
+        
     }
 
+    
 
     const array: string = elements.join(", ");
 
@@ -52,12 +107,13 @@ const BubleSortAnimation = (props: any) => {
  
              const heightStyle={height: +x*10 + 'px'};
              const itemId='item_' + i;
+             const numId = 'num_' + i;
              const blockId='block_' + i;
              const refEl:RefObject<HTMLDivElement> = React.createRef();
              items.push(refEl);
              return (
                      <div id={itemId} key={i} className="arrayItem">
-                         <div ref={refEl} className={classes.NumberItem}>{x}</div>
+                         <div id={numId} ref={refEl} className={classes.NumberItem}>{x}</div>
                          <div id={blockId} className={classes.Item} style={heightStyle}></div>
                      </div>
                  );
